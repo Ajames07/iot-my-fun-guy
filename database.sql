ba99af374cab98de011d8f3e63756b01636b8d34
Tr4vi5 <trav.dunn@outlook.com>
2018-10-27 09:53:01 -0500

    database create tables added

--- database.sql
+++ database.sql
@@ -1,5 +1,40 @@
 CREATE TABLE person (
     id SERIAL PRIMARY KEY,
     username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL
+    password VARCHAR (1000) NOT NULL,
+    notes VARCHAR (10000)
 );
+
+CREATE TABLE device (
+    id SERIAL PRIMARY KEY,
+    person_id INT REFERENCES person,
+    particle_id VARCHAR (10),
+    token VARCHAR (10),
+    location VARCHAR (100)
+);
+CREATE TABLE readings (
+    id SERIAL PRIMARY KEY,
+    device_id INT REFERENCES devices,
+    temperature VARCHAR (5),
+    humidity VARCHAR (4),
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+CREATE TABLE alerts (
+    id SERIAL PRIMARY KEY,
+    temperature_alert VARCHAR (5),
+    humidity_alert VARCHAR (4),
+    date TIMESTAMP NOT NULL CURRENT_TIMESTAMP,
+    device_id INT REFERENCES device
+);
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    note VARCHAR (1000),
+    person_id INT REFERENCES person
+);
+CREATE TABLE project_complete (
+    id SERIAL PRIMARY KEY,
+    temperature_data VARCHAR (5),
+    humidity_data VARCHAR (4),
+    yield VARCHAR (6)
+);
+
