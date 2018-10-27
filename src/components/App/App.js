01cf287da72537f6248ae93d5df63ee03fb5fb2b
Daniel MacKay <mackay2588@gmail.com>
2018-10-27 12:41:50 -0500

    removed user page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -41,11 +41,7 @@ class App extends Component {
               path="/about"
               component={AboutPage}
             />
-            <ProtectedRoute
-              exact
-              path="/profile"
-              component={UserPage}
-            />
+          
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
