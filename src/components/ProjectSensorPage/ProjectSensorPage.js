2c50e93e538352b892f06f0fe75280f62abc2904
Ajames07 <1silent0tter@gmail.com>
2018-10-27 14:34:21 -0500

    added state and componentDidMount to ProjectSensorPage, added connect to ProjectSensorPage

--- src/components/ProjectSensorPage/ProjectSensorPage.js
+++ src/components/ProjectSensorPage/ProjectSensorPage.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  sensorDataList: state.sensorDataList.SensorDataList
+});//end mapStateToProps
 
 class ProjectSensorPage extends Component {
+
+  state = {
+    sensorInfo: {
+        temperature: '',
+        humidity: '',
+        voc: '',
+        lux: '',
+        date: '',
+    },//end sensorInfo
+    //this is utilized in sensorEdit
+    sensorDataArray: [],
+    isUpdating: false,
+    updatingId: 0
+};//end state
+
+componentDidMount () {
+        this.props.dispatch({type: 'GET_DATA'});
+    }//end componentDidMount
+
   render() {
     return (
       <div>
         
       </div>
     )
-  }
-}
+  }//end render
+};//end ProjectSensorPage
 
-export default ProjectSensorPage
+export default connect(mapStateToProps)(ProjectSensorPage)
