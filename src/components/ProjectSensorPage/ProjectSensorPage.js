94d242981fa6c7ff80e4ca8c56fe77c604729c3f
Tr4vi5 <trav.dunn@outlook.com>
2018-10-27 11:20:23 -0500

    Projects table added to database

---
+++ src/components/ProjectSensorPage/ProjectSensorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProjectSensorPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ProjectSensorPage
